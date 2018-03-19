0ea66f10f788416197703d0cdfda9884cdb8ba67
Theresa Lee <tsanglee@yipitdata.com>
2018-03-19 09:06:10 -0400

    Use template context to show webpages

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -4,4 +4,24 @@ from django.shortcuts import render
 
 def home_page(request):
     """When you go to url, takes request and returns response."""
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Hello World!",
+        "content": "Welcome to the home page"
+    }
+    return render(request, "home_page.html", context)
+
+
+def about_page(request):
+    """When you go to url, takes request and returns response."""
+    context = {
+        "title": "About Page!"
+    }
+    return render(request, "home_page.html", context)
+
+
+def contact_page(request):
+    """When you go to url, takes request and returns response."""
+    context = {
+        "title": "Contact Page!"
+    }
+    return render(request, "home_page.html", context)

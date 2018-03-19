3b481618bde89f9526b9813dfef521d8f0c1dacf
Theresa Lee <tsanglee@yipitdata.com>
2018-03-19 09:05:42 -0400

    Use bootstrap template to show homepage

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def home_page(request):
     """When you go to url, takes request and returns response."""
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home_page.html", {})

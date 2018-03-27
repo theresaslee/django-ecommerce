73cf0371ace21ce56e908b0d0ec707ff395aacb5
Theresa Lee <tsanglee@yipitdata.com>
2018-03-27 09:21:55 -0400

    Add ListView

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.views.generic import ListView
 from django.shortcuts import render
 
+from .models import Product
+
+
 # Create your views here.
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    # default will be product_list by html, but you can alter
+    # template_name = 'products/list.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        return context
+
+# Same as above
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+    'object_list': queryset
+    }
+    return render(request, 'products/product_list.html', context)

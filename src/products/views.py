2d99b783d082da7af50ab3ea0833bf97fc10980a
Theresa Lee <tsanglee@yipitdata.com>
2018-03-29 09:01:11 -0400

    Create detail view

--- src/products/views.py
+++ src/products/views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
@@ -13,6 +13,7 @@ class ProductListView(ListView):
     # default will be product_list by html, but you can alter
     # template_name = 'products/list.html'
 
+    # same as context dict
     def get_context_data(self, *args, **kwargs):
         context = super(ProductListView, self).get_context_data(*args, **kwargs)
         return context
@@ -24,3 +25,23 @@ def product_list_view(request):
     'object_list': queryset
     }
     return render(request, 'products/product_list.html', context)
+
+# Create your views here.
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    # default will be product_list by html, but you can alter
+    # template_name = 'products/list.html'
+
+    # same as context dict
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        return context
+
+# Same as above
+def product_detail_view(request, pk=None):
+    instance = get_object_or_404(Product, pk=pk)
+    # instance = Product.objects.all(pk=pk) #id
+    context = {
+    'object': instance
+    }
+    return render(request, 'products/product_detail.html', context)
